f79a8610cca39f9d8964b8f68fec094f7320e672
Adrian Petrescu <adrian@apetre.sc>
2020-12-15 22:36:27 -0500

    adventofcode: add solution to 2020/15

---
+++ adventofcode/2020/15/15a.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+import sys
+
+seq = list(map(int, sys.stdin.read().split(',')))
+history = defaultdict(list)
+
+for i, n in enumerate(seq):
+    history[n].append(i)
+
+while len(seq) < 2020:
+    i += 1
+    if len(history[seq[-1]]) == 1:
+        n = 0
+    else:
+        n = history[seq[-1]][-1] - history[seq[-1]][-2]
+    seq.append(n)
+    history[n].append(i)
+
+print(history)
+print(seq)
