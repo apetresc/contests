4a9e6256fea5ae569bc9f1d86f475669990e9630
Adrian Petrescu <adrian@apetre.sc>
2020-12-20 22:29:13 -0500

    adventofcode: add solution for 2020/18

---
+++ adventofcode/2020/18/18b.py
@@ -0,0 +1,29 @@
+import math
+import sys
+from typing import List
+
+
+def tokenize(l : str):
+    return l.replace('(', '( ').replace(')', ' )').split()
+
+def parse_expr(e : List[str]) -> int:
+    assert e[0] == '('
+    e.pop(0)
+
+    acc = []
+    op = None
+    while e[0] != ')':
+        if e[0] == '(':
+            acc.append(parse_expr(e))
+        elif e[0] in ['*', '+']:
+            op = e.pop(0)
+            continue
+        else:
+            acc.append(int(e.pop(0)))
+        if len(acc) >= 2 and op == '+':
+            acc.append(acc.pop() + acc.pop())
+    assert e[0] == ')'
+    e.pop(0)
+    return math.prod(acc)
+
+print(sum(map(lambda l: parse_expr(tokenize('(' + l.strip() + ')')), sys.stdin.readlines())))
