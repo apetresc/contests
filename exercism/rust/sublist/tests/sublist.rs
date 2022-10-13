4e3b84217a9c282dd2132ade1b4d675265209c49
Adrian Petrescu <adrian@apetre.sc>
2022-10-13 18:14:26 -0400

    rust exercism: add test case to sublist that stumps my algorithm

--- exercism/rust/sublist/tests/sublist.rs
+++ exercism/rust/sublist/tests/sublist.rs
@@ -67,6 +67,15 @@ fn huge_sublist_not_in_huge_list() {
 }
 
 #[test]
+#[ignore]
+fn huge_sublist_not_in_huge_list_2() {
+    let v1: Vec<u64> = vec![0; 1_000_000];
+    let mut v2: Vec<u64> = vec![0; 500_000];
+    v2.push(1);
+    assert_eq!(Comparison::Unequal, sublist(&v1, &v2));
+}
+
+#[test]
 fn superlist_at_start() {
     assert_eq!(Comparison::Superlist, sublist(&[1, 2, 3, 4, 5], &[1, 2, 3]));
 }
