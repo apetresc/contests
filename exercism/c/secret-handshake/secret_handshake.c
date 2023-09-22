00cd3e4484f8f9beef336ef9facdb5751069d261
Adrian Petrescu <adrian@apetre.sc>
2023-09-21 20:59:52 -0400

    exercism: fix memory issue in C secret-handshake

--- exercism/c/secret-handshake/secret_handshake.c
+++ exercism/c/secret-handshake/secret_handshake.c
@@ -2,13 +2,15 @@
 
 #include <stdlib.h>
 
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
 const char **commands(size_t number) {
   short rev = (number >= (1 << 4)) ? -1 : 1;
   number &= ~(1 << 4);
   short n = __builtin_popcount(number);
   const char *events[] = {"wink", "double blink", "close your eyes", "jump"};
 
-  const char **commands = malloc(n * sizeof(*commands));
+  const char **commands = calloc(MAX(n, 1), sizeof(*commands));
   for (short i = (rev == 1) ? 0 : n - 1; i < n && i >= 0; i += rev) {
     short offset = __builtin_ctz(number);
     number &= ~(1 << offset);
